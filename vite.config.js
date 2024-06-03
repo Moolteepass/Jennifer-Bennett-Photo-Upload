1bd8e6bf2aafd104a7dd07ef4c0a2f9734ed95e1
Quabmac <mccormack.jacob@gmail.com>
2024-06-03 12:20:25 +1000

    take 2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/jennifer-bennett-photo-upload",
+  base: "Jennifer-Bennett-Photo-Upload/",
   plugins: [react()],
 })
