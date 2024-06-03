323e07dddcbedec93c85637e7580aed6b0c07018
Quabmac <mccormack.jacob@gmail.com>
2024-06-03 12:57:44 +1000

    secure url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "Jennifer-Bennett-Photo-Upload/",
+  base: "/Jennifer-Bennett-Photo-Upload/",
   plugins: [react()],
 })
